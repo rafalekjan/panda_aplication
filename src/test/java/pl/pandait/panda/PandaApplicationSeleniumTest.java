00ceba1b734870cfed663e60da9ccbd4a9e0cbf1
Your Name <you@example.com>
2020-11-09 17:51:59 +0000

    new tests

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -1,42 +1,37 @@
 package pl.pandait.panda;
-
-
+import java.net.URL;
+import java.net.MalformedURLException;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-
 import static org.junit.Assert.assertEquals;
-
 
 @SpringBootTest(classes = {PandaApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class PandaApplicationSeleniumTest {
-
+    
     private static WebDriver driver;
-
+    
     @LocalServerPort
     private int port;
-
+    
     @BeforeEach
-    public void startup() throws InterruptedException {
-
-        //Driver znajduje siÄ w resource
-        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
-        //ÅcieÅ¼ka do Firefoxa - jeÅ¼eli nie dziaÅa trzeba sprawdziÄ, gdzie FF jest zainstalowany!
-        System.setProperty("webdriver.firefox.bin", "/usr/lib/firefox/firefox");
-
-        // Tworzymy new
-        FirefoxOptions options = new FirefoxOptions();
-        options.addArguments("--headless");
-        driver = new FirefoxDriver(options);
-
-        // PamiÄtaj, Å¼e aplikacja Spring musi dziaÅaÄ! To znaczy teÅ¼ musi byÄ wÅÄczona.
+    public void startup() throws InterruptedException, MalformedURLException {
+        
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setPlatform(Platform.LINUX);
+        driver = new RemoteWebDriver(new URL("http://192.168.44.44:4444/wd/hub"), capabilities);
+        // Pamiętaj, że aplikacja Spring musi działać! To znaczy też musi być włączona.
+        
         driver.get(String.format("http://192.168.44.44:%d/", port));
 
         //Czekamy 2 sekundy
@@ -45,7 +40,7 @@ public class PandaApplicationSeleniumTest {
 
     @Test
     public void greetings_shouldOpenMainPageThenReturnWelcomeText() {
-        System.out.println("Uruchamiam test 1: Sprawdzenie napisu na stronie gÅÃ³wnej");
+        System.out.println("Uruchamiam test 1: Sprawdzenie napisu na stronie głównej");
         WebElement greetingElement = driver.findElement(By.xpath("//p"));
         String greetingText = greetingElement.getText().trim();
         assertEquals("Get your greeting here", greetingText);
@@ -66,6 +61,6 @@ public class PandaApplicationSeleniumTest {
 
     @AfterEach
     public void after() {
-        driver.close();
+        driver.quit();
     }
 }
